c8995e192c03095df0462b885f3f05aad6a947ef
Nonabona <bloodo9@hotmail.com>
2017-08-07 22:53:51 -0400

    map xml

--- Drinkr/Fragments/DrinkDetailFragment.cs
+++ Drinkr/Fragments/DrinkDetailFragment.cs
@@ -15,6 +15,11 @@ namespace Drinkr.Fragments
 {
     public class DrinkDetailFragment : Android.Support.V4.App.Fragment
     {
+        Button btnGetRecipe;
+        Button btnFindNearBy;
+        FrameLayout frameLayout;
+        LinearLayout linearLayout;
+
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -24,10 +29,54 @@ namespace Drinkr.Fragments
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
+            
+
             // Use this to return your custom view for this Fragment
             View view  = inflater.Inflate(Resource.Layout.DrinkDetail, container, false);
 
+            //linearLayout = view.FindViewById<LinearLayout>(Resource.Id.llFinder);
+            frameLayout = view.FindViewById<FrameLayout>(Resource.Id.drinkDetailFragContainer);
+            btnFindNearBy = view.FindViewById<Button>(Resource.Id.btnFindNearBy);
+            btnGetRecipe = view.FindViewById<Button>(Resource.Id.btnFindRecipe);
+
+            btnGetRecipe.Click += BtnGetRecipe_Click;
+            btnFindNearBy.Click += BtnFindNearBy_Click;
+
+            //frameLayout.Visibility = ViewStates.Gone;
+          
             return view;
+        }
+
+        private void BtnGetRecipe_Click(object sender, EventArgs e)
+        {
+            //linearLayout.Visibility = ViewStates.Visible;
+            frameLayout.Visibility = ViewStates.Visible;
+            RecipeFragment rf = new RecipeFragment();
+            ShowFragment(rf);
+        }
+
+        private void BtnFindNearBy_Click(object sender, EventArgs e)
+        {
+            //linearLayout.Visibility = ViewStates.Visible;
+            frameLayout.Visibility = ViewStates.Visible;
+            MapFragment mf = new MapFragment();
+            ShowFragment(mf);
+        }
+
+        private void ShowFragment(Android.Support.V4.App.Fragment frag)
+        {
+            if (!frag.IsVisible)
+            {
+                var trans = FragmentManager.BeginTransaction();
+
+                trans.Add(Resource.Id.fragmentContainer, frag);
+
+                //trans.Hide(currSelectedFrag);
+                trans.Show(frag);
+                trans.AddToBackStack(null);
+                trans.Commit();
+
+            }
         }
     }
 }
---
+++ Drinkr/Fragments/MapFragment.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.Gms.Maps;
+using Android.Support.V4.App;
+using Android.Gms.Maps.Model;
+
+namespace Drinkr.Fragments
+{
+    public class MapFragment : Android.Support.V4.App.Fragment, IOnMapReadyCallback
+    {
+        private GoogleMap map;
+         
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            View view = inflater.Inflate(Resource.Layout.Map, container, false);
+
+
+
+            SupportMapFragment mapFrag = ((SupportMapFragment)FragmentManager.FindFragmentById(Resource.Id.map));
+
+            if (mapFrag == null)
+            {
+                GoogleMapOptions gmo = new GoogleMapOptions()
+                    .InvokeMapType(GoogleMap.MapTypeNormal)
+                    .InvokeZoomControlsEnabled(false)
+                    .InvokeCompassEnabled(true);
+
+                Android.Support.V4.App.FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+                mapFrag = SupportMapFragment.NewInstance(gmo);
+                fragTx.Add(Resource.Id.map, mapFrag, "map");
+                fragTx.Commit();
+
+            }
+
+            mapFrag.GetMapAsync(this);
+
+            return view;
+
+        }
+
+        public void OnMapReady(GoogleMap googleMap)
+        {
+            map = googleMap;
+
+            if (map != null)
+            {
+                LatLng currPosition = new LatLng(40.71, -74.00);
+
+                MarkerOptions markerOpt1 = new MarkerOptions();
+                markerOpt1.SetPosition(currPosition);
+                markerOpt1.SetTitle("Vimy Ridge");
+                //map.MapType = GoogleMap.MapTypeSatellite;
+
+                //CameraPosition cp = new CameraPosition(currPosition, 14, 0, 0);
+                CameraUpdate cu = CameraUpdateFactory.NewLatLngZoom(currPosition, 14);
+                map.MoveCamera(cu);
+                map.AddMarker(markerOpt1);
+            }
+
+            
+            
+        }
+    }
+}
